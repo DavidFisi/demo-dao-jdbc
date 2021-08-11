8103ca32e28f5a8e4581b3ebd6c0c3d19161af32
DavidFisi <jrfisicaro@gmail.com>
2021-08-11 11:44:42 -0300

    Ajuste

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,11 +12,11 @@ public class Program {
 	public static void main(String[] args) {
 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		//System.out.println("=== TEST 1: seller findbyId ===");
-		//Seller seller = sellerDao.findById(3);
 		
-		//System.out.println(seller);
-		//
+		System.out.println("=== TEST 1: seller findbyId ===");
+		Seller seller = sellerDao.findById(3);
+		System.out.println(seller);
+		
 		System.out.println("\n=== TEST 2: seller findbyDepartment ===");
 		Department department = new Department(2,null);
 		List<Seller> list = sellerDao.findbyDepartment(department);
